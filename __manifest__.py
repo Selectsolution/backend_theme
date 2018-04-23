6d2786d3f941f6a023e57b9571e3169f2cb9a36b
Ahmad Nabeel <nabeel@select-online.net>
2018-04-23 18:06:20 +0200

    first publish
    first publish

---
+++ __manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Openworx, LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Enterprise odoo Theme By Select Solutions",
+    "summary": "Odoo 11.0 Backend Theme Converting your Community Version into Enterprise User Interface",
+    "version": "1.0",
+    "category": "Themes/Backend",
+    "website": "http://Select-Online.net",
+        "description": """
+             Odoo 11.0 Backend Theme Converting your Community Version into Enterprise User Interface.
+             More polished and added some responsive CSS rules.
+             For More Apps and Themes Visit Us On www.select-online.net
+    """,
+        'images':[
+        'images/screen.png'
+        ],
+    "author": "Select Solutions",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        'web',
+    ],
+    "data": [
+        'views/assets.xml',
+        'views/web.xml',
+    ],
+}
